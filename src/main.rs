9ed40c73558396185fa52d8197aac4490d90ca69
Nicholas Sandomeno <nicholassandomeno@Nicholass-MBP.fios-router.home>
2023-01-08 13:53:25 -0500

    loop for multiple connections

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,30 @@ async fn main() {
     // To explore, remove .await from end of line and check
     // type of listener.
     let listener = TcpListener::bind("localhost:5000").await.unwrap();
-    // Accept a connection
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-        // Split the read and write sides of the socket
-        let (reader, mut writer) = socket.split();
-        // Store for the data passed to the TCP Socket
-        let mut reader = BufReader::new(reader);
-        let mut line = String::new();
+    // Loop that enables multiple clients to connect
     loop {
-        // Read the bytes from the steam to the buffer and return
-        // the number of bytes read
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 {
-            break;
-        }
-        // Echo the entire contents back to the client
-        // since the byte stream could be smaller than the size of the buffer,
-        // we will use write_all to truncate the space in the buffer that was unused
+        // Accept a connection
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+            // Split the read and write sides of the socket
+            let (reader, mut writer) = socket.split();
+            // Store for the data passed to the TCP Socket
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+        loop {
+            // Read the bytes from the steam to the buffer and return
+            // the number of bytes read
+            let bytes_read = reader.read_line(&mut line).await.unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+            // Echo the entire contents back to the client
+            // since the byte stream could be smaller than the size of the buffer,
+            // we will use write_all to truncate the space in the buffer that was unused
 
-        // Pass in the buffer as the source of data for the write back to the client
-        // through the socket, up to the number of bytes previously read by the socket.
-        writer.write_all(&mut line.as_bytes()).await.unwrap();
-        line.clear();
+            // Pass in the buffer as the source of data for the write back to the client
+            // through the socket, up to the number of bytes previously read by the socket.
+            writer.write_all(&mut line.as_bytes()).await.unwrap();
+            line.clear();
+        }
     }
 }
