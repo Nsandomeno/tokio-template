4c741c9461ec5e7fbd0fdd5ebbfe001738d7cbe0
Nicholas Sandomeno <nicholassandomeno@Nicholass-MBP.fios-router.home>
2023-01-09 07:58:27 -0500

    sending and receiving broadcast channel messages with async tasks

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,16 @@ async fn main() {
     let listener = TcpListener::bind("localhost:5000").await.unwrap();
     // Broadcast channel for chat server - multiple producers/consumers on a single channel
     // In our case this means we have a sender/receiver for every async task
-    let (tx, rx) = broadcast::channel::<String>(MAX_BROADCAST_CHANNELS);
+    let (tx, _rx) = broadcast::channel::<String>(MAX_BROADCAST_CHANNELS);
     // Loop that enables multiple clients to connect
     loop {
         // Accept a connection
         let (mut socket, _addr) = listener.accept().await.unwrap();
+            // Clone the sender of the broadcast channel so that it can be moved into the async task
+            let tx = tx.clone();
+            // Subscribe to the broadcast sender with a receiver of the broadcast channel. Done so
+            // that it too can be moved into the async task
+            let mut rx = tx.subscribe();
             // Spawn a new task (think concept of multi-threading applied within a single thread)
             tokio::spawn(async move {
                 // Split the read and write sides of the socket
@@ -31,13 +36,18 @@ async fn main() {
                     if bytes_read == 0 {
                         break;
                     }
+                    // Sending the read line from the async task (sinc the sender of the broadcast channel was moved into the block) 
+                    tx.send(line.clone()).unwrap();
+
+                    // Receiving messages 
+                    let msg = rx.recv().await.unwrap();
                     // Echo the entire contents back to the client
                     // since the byte stream could be smaller than the size of the buffer,
                     // we will use write_all to truncate the space in the buffer that was unused
 
                     // Pass in the buffer as the source of data for the write back to the client
                     // through the socket, up to the number of bytes previously read by the socket.
-                    writer.write_all(&mut line.as_bytes()).await.unwrap();
+                    writer.write_all(&mut msg.as_bytes()).await.unwrap();
                     line.clear();
                 }
             });
